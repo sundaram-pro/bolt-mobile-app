ac18f6c7ce05309207d07c47d7800867b47d6a79
sundaram-pro <sundaram.pro@outlook.com>
2025-03-26 01:44:58 +0530

    auth using clerk

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,16 +1,17 @@
 import { prismaClient } from '@prisma/client';
 import cors from 'cors';
 import express from 'express';
+import { authMiddleware } from './middleware';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 
-app.post("/project", async (req, res) => {
+app.post("/project", authMiddleware, async (req, res) => {
 
     const { prompt } = req.body;
-    const userId = req.userId;
+    const userId = req.userId!;
     const description = prompt.split("\n")[0];
     const project = await prismaClient.project.create({
         data: {
@@ -20,9 +21,9 @@ app.post("/project", async (req, res) => {
     res.json({projectId: project.id});
 })
 
-app.get("/projects", async (req, res) => {
-    const userId = req.userId;
-    const project = await prismaClient.project.findUnique({
+app.get("/projects",authMiddleware, async (req, res) => {
+    const userId = req.userId!;
+    const project = await prismaClient.project.findFirst({
         where: { id: projectId },
     });
 
--- apps/primary-backend/middleware.ts
+++ apps/primary-backend/middleware.ts
@@ -1,15 +1,27 @@
 import type { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-export function authMiddleware(req: Request, res: Response , next: NextFunction) {
+export function authMiddleware(rcdeq: Request, res: Response , next: NextFunction) {
   const token = req.headers.authorization;
   if(!token) {
-    return res.status(401).json({error: 'Unauthorized'});
+    res.status(401).json({error: 'Unauthorized'});
+    return;
   }
 
-  const decoded = jwt.verify(token, process.env.JWT_PUBLIC_KEY);
+  const decoded = jwt.verify(token, process.env.JWT_PUBLIC_KEY!,{
+    algorithms: ['RS256'],
+  });
+
   if(!decoded){
-    return res.status(401).json({error: 'Unauthorized'});
+    res.status(401).json({error: 'Unauthorized'});
+    return;
+  }
+
+  const userId = decoded.userId;
+
+  if(!userId){
+    res.status(401).json({error: 'Unauthorized'});
+    return;
   }
 
   req.userId = decoded.userId;
